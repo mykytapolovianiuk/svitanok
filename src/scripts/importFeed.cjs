c6f907cbfa0f9f177e34287ec3cfd8a69db1b2ce
mykytapolovianiuk <mykytapolovianiuk@gmail.com>
2025-12-18 17:13:28 +0200

    Update import script to use JavaScript modules

--- src/scripts/importFeed.cjs
+++ src/scripts/importFeed.cjs
@@ -16,7 +16,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Load the XML parser function
-const { parseYML } = require('../utils/xmlParser');
+const { parseYML } = require('../utils/xmlParser.js');
 
 // Load environment variables
 const supabaseUrl = process.env.VITE_SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL;
